c99c4842a6c032d31d03441613e58d056df0db75
Denis <133989468+ollias13@users.noreply.github.com>
2023-05-19 17:08:19 +0300

    Add files via upload

---
+++ LB10/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace LB10
+{
+    public partial class Form1 : Form
+    {
+        // Семафор для синхронізації доступу до графічного контексту
+        private Semaphore semaphore = new Semaphore(1, 1);
+
+        // Об'єкти ManualResetEvent для призупинення потоків
+        private ManualResetEvent pauseThreads = new ManualResetEvent(true);
+        private ManualResetEvent pauseThread1 = new ManualResetEvent(true);
+        private ManualResetEvent pauseThread2 = new ManualResetEvent(true);
+        private ManualResetEvent pauseThread3 = new ManualResetEvent(true);
+
+        // Потоки
+        Thread thread1;
+        Thread thread2;
+        Thread thread3;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Ініціалізація потоків
+            thread1 = new Thread(new ThreadStart(draw_rect));
+            thread2 = new Thread(new ThreadStart(draw_eclips));
+            thread3 = new Thread(new ThreadStart(Rnd_num));
+        }
+
+        private void draw_rect()
+        {
+            try
+            {
+                Random rnd = new Random();
+                Graphics g = panel1.CreateGraphics();
+                while (true)
+                {
+                    semaphore.WaitOne();
+                    pauseThread1.WaitOne();
+                    Thread.Sleep(40);
+                    g.DrawRectangle(Pens.Pink, 0, 0, rnd.Next(this.Width), rnd.Next(this.Height));
+                    semaphore.Release();
+                }
+            }
+            catch (Exception) { }
+        }
+
+        private void draw_eclips()
+        {
+            try
+            {
+                Random rnd = new Random();
+                Graphics g = panel2.CreateGraphics();
+                while (true)
+                {
+                    semaphore.WaitOne();
+                    pauseThread1.WaitOne();
+                    Thread.Sleep(40);
+                    g.DrawEllipse(Pens.Pink, 0, 0, rnd.Next(this.Width), rnd.Next(this.Height));
+                    semaphore.Release();
+                }
+            }
+            catch (Exception) { }
+        }
+
+        private void Rnd_num()
+        {
+            try
+            {
+                Random rnd = new Random();
+                Parallel.For(0, 500, i =>
+                {
+                    richTextBox1.Invoke((MethodInvoker)delegate ()
+                    {
+                        richTextBox1.Text += rnd.Next().ToString();
+                    });
+                });
+            }
+            catch (Exception) { }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // Запуск усіх потоків
+            thread1.Start();
+            thread2.Start();
+            thread3.Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Запуск потоку 1
+            thread1.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Запуск потоку 2
+            thread2.Start();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Запуск потоку 3
+            thread3.Start();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            // Призупинення потоку 1
+            pauseThread1.Reset();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            // Призупинення потоку 2
+            pauseThread2.Reset();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            // Призупинення потоку 3
+            pauseThread3.Reset();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            // Призупинення всіх потоків
+            pauseThreads.Reset();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            // Завершення потоків при закритті форми
+            thread1.Abort();
+            thread2.Abort();
+            thread3.Abort();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            // Відображення форми Form2
+            Form2 form2 = new Form2();
+            form2.Show();
+        }
+    }
+}
---
+++ LB10/Form2.Designer.cs
@@ -0,0 +1,150 @@
+﻿
+namespace LB10
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.encryptButton = new System.Windows.Forms.Button();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.redocTextBox = new System.Windows.Forms.TextBox();
+            this.shaTextBox = new System.Windows.Forms.TextBox();
+            this.lucTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // encryptButton
+            // 
+            this.encryptButton.Location = new System.Drawing.Point(340, 30);
+            this.encryptButton.Name = "encryptButton";
+            this.encryptButton.Size = new System.Drawing.Size(256, 77);
+            this.encryptButton.TabIndex = 0;
+            this.encryptButton.Text = "ОК";
+            this.encryptButton.UseVisualStyleBackColor = true;
+            this.encryptButton.Click += new System.EventHandler(this.encryptButton_Click);
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(28, 30);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(306, 22);
+            this.inputTextBox.TabIndex = 1;
+            // 
+            // redocTextBox
+            // 
+            this.redocTextBox.Location = new System.Drawing.Point(28, 113);
+            this.redocTextBox.Name = "redocTextBox";
+            this.redocTextBox.Size = new System.Drawing.Size(568, 22);
+            this.redocTextBox.TabIndex = 2;
+            // 
+            // shaTextBox
+            // 
+            this.shaTextBox.Location = new System.Drawing.Point(28, 158);
+            this.shaTextBox.Name = "shaTextBox";
+            this.shaTextBox.Size = new System.Drawing.Size(568, 22);
+            this.shaTextBox.TabIndex = 3;
+            // 
+            // lucTextBox
+            // 
+            this.lucTextBox.Location = new System.Drawing.Point(28, 203);
+            this.lucTextBox.Name = "lucTextBox";
+            this.lucTextBox.Size = new System.Drawing.Size(568, 22);
+            this.lucTextBox.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Введіть текст";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "REDOC";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "SHA";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 183);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "LUC";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(608, 252);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lucTextBox);
+            this.Controls.Add(this.shaTextBox);
+            this.Controls.Add(this.redocTextBox);
+            this.Controls.Add(this.inputTextBox);
+            this.Controls.Add(this.encryptButton);
+            this.Name = "Form2";
+            this.Text = "LAB10.2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button encryptButton;
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.TextBox redocTextBox;
+        private System.Windows.Forms.TextBox shaTextBox;
+        private System.Windows.Forms.TextBox lucTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
