b018882fe568cde74b793cde6e631269c4e8732f
Denis <133989468+ollias13@users.noreply.github.com>
2023-05-19 17:08:55 +0300

    Add files via upload

---
+++ LAB11/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿
+namespace LAB11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InsertDataButton = new System.Windows.Forms.Button();
+            this.DisplayDataButton = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.surnameTextBox = new System.Windows.Forms.TextBox();
+            this.positionTextBox = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.birthDateDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.ReportButton = new System.Windows.Forms.Button();
+            this.reportRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.searchResultsRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.reportRichTextBox2 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // InsertDataButton
+            // 
+            this.InsertDataButton.Location = new System.Drawing.Point(118, 11);
+            this.InsertDataButton.Name = "InsertDataButton";
+            this.InsertDataButton.Size = new System.Drawing.Size(340, 23);
+            this.InsertDataButton.TabIndex = 0;
+            this.InsertDataButton.Text = "Внести дані";
+            this.InsertDataButton.UseVisualStyleBackColor = true;
+            this.InsertDataButton.Click += new System.EventHandler(this.InsertDataButton_Click);
+            // 
+            // DisplayDataButton
+            // 
+            this.DisplayDataButton.Location = new System.Drawing.Point(118, 39);
+            this.DisplayDataButton.Name = "DisplayDataButton";
+            this.DisplayDataButton.Size = new System.Drawing.Size(340, 23);
+            this.DisplayDataButton.TabIndex = 1;
+            this.DisplayDataButton.Text = "Показати таблицю";
+            this.DisplayDataButton.UseVisualStyleBackColor = true;
+            this.DisplayDataButton.Click += new System.EventHandler(this.DisplayDataButton_Click);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(482, 11);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowHeadersWidth = 51;
+            this.dataGridView.RowTemplate.Height = 24;
+            this.dataGridView.Size = new System.Drawing.Size(606, 481);
+            this.dataGridView.TabIndex = 2;
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(12, 12);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(100, 22);
+            this.nameTextBox.TabIndex = 3;
+            // 
+            // surnameTextBox
+            // 
+            this.surnameTextBox.Location = new System.Drawing.Point(12, 40);
+            this.surnameTextBox.Name = "surnameTextBox";
+            this.surnameTextBox.Size = new System.Drawing.Size(100, 22);
+            this.surnameTextBox.TabIndex = 4;
+            // 
+            // positionTextBox
+            // 
+            this.positionTextBox.Location = new System.Drawing.Point(-287, 368);
+            this.positionTextBox.Name = "positionTextBox";
+            this.positionTextBox.Size = new System.Drawing.Size(100, 22);
+            this.positionTextBox.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 68);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(-287, 266);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 7;
+            // 
+            // birthDateDateTimePicker
+            // 
+            this.birthDateDateTimePicker.Location = new System.Drawing.Point(12, 96);
+            this.birthDateDateTimePicker.Name = "birthDateDateTimePicker";
+            this.birthDateDateTimePicker.Size = new System.Drawing.Size(248, 22);
+            this.birthDateDateTimePicker.TabIndex = 8;
+            // 
+            // ReportButton
+            // 
+            this.ReportButton.Location = new System.Drawing.Point(12, 138);
+            this.ReportButton.Name = "ReportButton";
+            this.ReportButton.Size = new System.Drawing.Size(410, 23);
+            this.ReportButton.TabIndex = 9;
+            this.ReportButton.Text = "Показати запити";
+            this.ReportButton.UseVisualStyleBackColor = true;
+            this.ReportButton.Click += new System.EventHandler(this.ReportButton_Click);
+            // 
+            // reportRichTextBox
+            // 
+            this.reportRichTextBox.Location = new System.Drawing.Point(12, 168);
+            this.reportRichTextBox.Name = "reportRichTextBox";
+            this.reportRichTextBox.Size = new System.Drawing.Size(181, 111);
+            this.reportRichTextBox.TabIndex = 10;
+            this.reportRichTextBox.Text = "";
+            // 
+            // SearchButton
+            // 
+            this.SearchButton.Location = new System.Drawing.Point(13, 286);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(282, 23);
+            this.SearchButton.TabIndex = 11;
+            this.SearchButton.Text = "Пошук по прізвищю";
+            this.SearchButton.UseVisualStyleBackColor = true;
+            this.SearchButton.Click += new System.EventHandler(this.SearchButton_Click);
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Location = new System.Drawing.Point(301, 285);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(157, 22);
+            this.searchTextBox.TabIndex = 12;
+            // 
+            // searchResultsRichTextBox
+            // 
+            this.searchResultsRichTextBox.Location = new System.Drawing.Point(13, 316);
+            this.searchResultsRichTextBox.Name = "searchResultsRichTextBox";
+            this.searchResultsRichTextBox.Size = new System.Drawing.Size(445, 177);
+            this.searchResultsRichTextBox.TabIndex = 13;
+            this.searchResultsRichTextBox.Text = "";
+            // 
+            // reportRichTextBox2
+            // 
+            this.reportRichTextBox2.Location = new System.Drawing.Point(220, 168);
+            this.reportRichTextBox2.Name = "reportRichTextBox2";
+            this.reportRichTextBox2.Size = new System.Drawing.Size(202, 111);
+            this.reportRichTextBox2.TabIndex = 14;
+            this.reportRichTextBox2.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1103, 505);
+            this.Controls.Add(this.reportRichTextBox2);
+            this.Controls.Add(this.searchResultsRichTextBox);
+            this.Controls.Add(this.searchTextBox);
+            this.Controls.Add(this.SearchButton);
+            this.Controls.Add(this.reportRichTextBox);
+            this.Controls.Add(this.ReportButton);
+            this.Controls.Add(this.birthDateDateTimePicker);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.positionTextBox);
+            this.Controls.Add(this.surnameTextBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.DisplayDataButton);
+            this.Controls.Add(this.InsertDataButton);
+            this.Name = "Form1";
+            this.Text = "LAB11";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button InsertDataButton;
+        private System.Windows.Forms.Button DisplayDataButton;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox surnameTextBox;
+        private System.Windows.Forms.TextBox positionTextBox;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.DateTimePicker birthDateDateTimePicker;
+        private System.Windows.Forms.Button ReportButton;
+        private System.Windows.Forms.RichTextBox reportRichTextBox;
+        private System.Windows.Forms.Button SearchButton;
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.RichTextBox searchResultsRichTextBox;
+        private System.Windows.Forms.RichTextBox reportRichTextBox2;
+    }
+}
+
---
+++ LAB11/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LAB11
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Users\mrden\OneDrive\Рабочий стол\csharp\LAB11\db.mdb";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void InsertDataButton_Click(object sender, EventArgs e)
+        {
+            // Отримуємо дані з текстових полів
+            string name = nameTextBox.Text;
+            string surname = surnameTextBox.Text;
+            string position = positionTextBox.Text;
+            DateTime birthDateValue = birthDateDateTimePicker.Value;
+
+            using (OleDbConnection connection = new OleDbConnection(connectionString))
+            {
+                connection.Open();
+
+                // Запит на вставку даних
+                string insertQuery = "INSERT INTO Співробітники (Ім_я, Прізвище, Посада, Дата_народження) VALUES (?, ?, ?, ?)";
+
+                // Виконуємо команду з параметрами
+                using (OleDbCommand command = new OleDbCommand(insertQuery, connection))
+                {
+                    command.Parameters.AddWithValue("@p1", name);
+                    command.Parameters.AddWithValue("@p2", surname);
+                    command.Parameters.AddWithValue("@p3", position);
+                    command.Parameters.AddWithValue("@p4", birthDateValue.ToString("MM/dd/yyyy"));
+                    command.ExecuteNonQuery();
+                }
+
+                MessageBox.Show("Дані успішно вставлені до бази даних.");
+            }
+        }
+
+        private void DisplayDataButton_Click(object sender, EventArgs e)
+        {
+            using (OleDbConnection connection = new OleDbConnection(connectionString))
+            {
+                connection.Open();
+
+                // Отримуємо дані з бази даних
+                DataTable dataTable = RetrieveData(connection);
+
+                // Відображаємо дані в DataGridView
+                dataGridView.DataSource = dataTable;
+            }
+        }
+
+        private DataTable RetrieveData(OleDbConnection connection)
+        {
+            // Запит на отримання даних
+            string selectQuery = "SELECT * FROM Співробітники";
+
+            using (OleDbDataAdapter adapter = new OleDbDataAdapter(selectQuery, connection))
+            {
+                // Створюємо нову таблицю
+                DataTable dataTable = new DataTable();
+
+                // Заповнюємо таблицю даними з бази даних
+                adapter.Fill(dataTable);
+
+                return dataTable;
+            }
+        }
+
+        private void ReportButton_Click(object sender, EventArgs e)
+        {
+            using (OleDbConnection connection = new OleDbConnection(connectionString))
+            {
+                connection.Open();
+
+                // Звіт 1: Загальна інформація про співробітників
+                string reportQuery1 = "SELECT * FROM Співробітники";
+
+                using (OleDbCommand command = new OleDbCommand(reportQuery1, connection))
+                {
+                    using (OleDbDataReader reader = command.ExecuteReader())
+                    {
+                        StringBuilder report1 = new StringBuilder();
+
+                        // Читаємо дані і формуємо звіт
+                        while (reader.Read())
+                        {
+                            string employeeId = reader["Id"].ToString();
+                            string employeeName = reader["Ім_я"].ToString();
+                            string employeeSurname = reader["Прізвище"].ToString();
+                            string employeePosition = reader["Посада"].ToString();
+                            string employeeBirthDate = reader["Дата_народження"].ToString();
+
+                            report1.AppendLine($"ID: {employeeId}");
+                            report1.AppendLine($"Ім'я: {employeeName}");
+                            report1.AppendLine($"Прізвище: {employeeSurname}");
+                            report1.AppendLine($"Посада: {employeePosition}");
+                            report1.AppendLine($"Дата народження: {employeeBirthDate}");
+                            report1.AppendLine();
+                        }
+
+                        // Виводимо звіт 1 у RichTextBox
+                        reportRichTextBox.Text = report1.ToString();
+                    }
+                }
+
+                // Звіт 2: Звіт за критерієм вибору (наприклад, посада)
+                string reportQuery2 = "SELECT * FROM Співробітники WHERE Посада = ?";
+
+                using (OleDbCommand command = new OleDbCommand(reportQuery2, connection))
+                {
+                    command.Parameters.AddWithValue("@p0", "Менеджер"); // Замініть на вибраний критерій
+
+                    using (OleDbDataReader reader = command.ExecuteReader())
+                    {
+                        StringBuilder report2 = new StringBuilder();
+
+                        // Читаємо дані і формуємо звіт
+                        while (reader.Read())
+                        {
+                            string employeeId = reader["Id"].ToString();
+                            string employeeName = reader["Ім_я"].ToString();
+                            string employeeSurname = reader["Прізвище"].ToString();
+                            string employeePosition = reader["Посада"].ToString();
+                            string employeeBirthDate = reader["Дата_народження"].ToString();
+
+                            report2.AppendLine($"ID: {employeeId}");
+                            report2.AppendLine($"Ім'я: {employeeName}");
+                            report2.AppendLine($"Прізвище: {employeeSurname}");
+                            report2.AppendLine($"Посада: {employeePosition}");
+                            report2.AppendLine($"Дата народження: {employeeBirthDate}");
+                            report2.AppendLine();
+                        }
+
+                        // Виводимо звіт 2 у RichTextBox
+                        reportRichTextBox2.Text = report2.ToString();
+                    }
+                }
+            }
+        }
+
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+            string searchCriteria = searchTextBox.Text;
+
+            using (OleDbConnection connection = new OleDbConnection(connectionString))
+            {
+                connection.Open();
+
+                // Запит на пошук даних за критерієм
+                string searchQuery = "SELECT * FROM Співробітники WHERE Прізвище = ?";
+
+                using (OleDbCommand command = new OleDbCommand(searchQuery, connection))
+                {
+                    command.Parameters.AddWithValue("@p0", searchCriteria);
+
+                    using (OleDbDataReader reader = command.ExecuteReader())
+                    {
+                        StringBuilder searchResults = new StringBuilder();
+
+                        // Читаємо дані і формуємо результати пошуку
+                        while (reader.Read())
+                        {
+                            string employeeId = reader["Id"].ToString();
+                            string employeeName = reader["Ім_я"].ToString();
+                            string employeeSurname = reader["Прізвище"].ToString();
+                            string employeePosition = reader["Посада"].ToString();
+                            string employeeBirthDate = reader["Дата_народження"].ToString();
+
+                            searchResults.AppendLine($"ID: {employeeId}");
+                            searchResults.AppendLine($"Ім'я: {employeeName}");
+                            searchResults.AppendLine($"Прізвище: {employeeSurname}");
+                            searchResults.AppendLine($"Посада: {employeePosition}");
+                            searchResults.AppendLine($"Дата народження: {employeeBirthDate}");
+                            searchResults.AppendLine();
+                        }
+
+                        // Виводимо результати пошуку у RichTextBox
+                        searchResultsRichTextBox.Text = searchResults.ToString();
+                    }
+                }
+            }
+        }
+    }
+}
