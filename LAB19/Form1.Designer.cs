24839aa761166e5c45b2c791d7c142ebfaf77643
Denis <133989468+ollias13@users.noreply.github.com>
2023-05-21 19:58:10 +0300

    Add files via upload

---
+++ LAB19/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace LAB19
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.listBoxHardwareInfo = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(13, 13);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(775, 23);
+            this.btnRefresh.TabIndex = 0;
+            this.btnRefresh.Text = "Оновити";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // listBoxHardwareInfo
+            // 
+            this.listBoxHardwareInfo.FormattingEnabled = true;
+            this.listBoxHardwareInfo.ItemHeight = 16;
+            this.listBoxHardwareInfo.Location = new System.Drawing.Point(13, 43);
+            this.listBoxHardwareInfo.Name = "listBoxHardwareInfo";
+            this.listBoxHardwareInfo.Size = new System.Drawing.Size(775, 388);
+            this.listBoxHardwareInfo.TabIndex = 1;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBoxHardwareInfo);
+            this.Controls.Add(this.btnRefresh);
+            this.Name = "MainForm";
+            this.Text = "Лабораторна робота 19";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.ListBox listBoxHardwareInfo;
+    }
+}
+
---
+++ LAB19/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Management;
+
+namespace LAB19
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            // При завантаженні форми виконуємо отримання і виведення інформації
+            GetAndDisplayHardwareInformation();
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            // Обробник події натискання кнопки "Оновити"
+            GetAndDisplayHardwareInformation();
+        }
+
+        private void GetAndDisplayHardwareInformation()
+        {
+            // Отримання і виведення інформації про комп'ютер
+
+            // Очищаємо попередні дані
+            ClearInformation();
+
+            // Отримуємо інформацію про процесор
+            DisplayHardwareInfo("Процесор:", GetHardwareInfo("Win32_Processor", "Name"));
+            DisplayHardwareInfo("Виробник:", GetHardwareInfo("Win32_Processor", "Manufacturer"));
+            DisplayHardwareInfo("Опис:", GetHardwareInfo("Win32_Processor", "Description"));
+
+            // Отримуємо інформацію про відеокарту
+            DisplayHardwareInfo("Відеокарта:", GetHardwareInfo("Win32_VideoController", "Name"));
+            DisplayHardwareInfo("Видеопроцесор:", GetHardwareInfo("Win32_VideoController", "VideoProcessor"));
+            DisplayHardwareInfo("Версія драйверу:", GetHardwareInfo("Win32_VideoController", "DriverVersion"));
+            DisplayHardwareInfo("Об’єм пам’яти (в байтах):", GetHardwareInfo("Win32_VideoController", "AdapterRAM"));
+
+            // Отримуємо інформацію про DVD привід
+            DisplayHardwareInfo("Назва DVD:", GetHardwareInfo("Win32_CDROMDrive", "Name"));
+            DisplayHardwareInfo("Буква DVD:", GetHardwareInfo("Win32_CDROMDrive", "Drive"));
+
+            // Отримуємо інформацію про жорсткий диск
+            DisplayHardwareInfo("Жорстикий диск:", GetHardwareInfo("Win32_DiskDrive", "Caption"));
+            DisplayHardwareInfo("Об’єм (в байтах):", GetHardwareInfo("Win32_DiskDrive", "Size"));
+
+            // Інформація про материнську плату
+            DisplayHardwareInfo("Материнська плата:", GetHardwareInfo("Win32_BaseBoard", "Product"));
+            DisplayHardwareInfo("Виробник материнської плати:", GetHardwareInfo("Win32_BaseBoard", "Manufacturer"));
+            DisplayHardwareInfo("Серійний номер материнської плати:", GetHardwareInfo("Win32_BaseBoard", "SerialNumber"));
+
+            // Інформація про мережеве обладнання
+            DisplayHardwareInfo("Мережева карта:", GetHardwareInfo("Win32_NetworkAdapter", "Name"));
+
+            // Інформація про BIOS
+            DisplayHardwareInfo("BIOS:", GetHardwareInfo("Win32_BIOS", "Caption"));
+            DisplayHardwareInfo("Версія BIOS:", GetHardwareInfo("Win32_BIOS", "Version"));
+            DisplayHardwareInfo("Виробник BIOS:", GetHardwareInfo("Win32_BIOS", "Manufacturer"));
+        }
+
+        private List<string> GetHardwareInfo(string win32Class, string classItemField)
+        {
+            // Метод для отримання інформації про компоненти
+
+            List<string> result = new List<string>();
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM " + win32Class);
+
+            try
+            {
+                foreach (ManagementObject obj in searcher.Get())
+                {
+                    if (obj[classItemField] != null)
+                    {
+                        result.Add(obj[classItemField].ToString().Trim());
+                    }
+                    else
+                    {
+                        result.Add("Немає даних");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Помилка при отриманні інформації: " + ex.Message, "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            return result;
+        }
+
+        private void DisplayHardwareInfo(string info, List<string> result)
+        {
+            // Виведення інформації на форму
+
+            if (!string.IsNullOrEmpty(info))
+            {
+                listBoxHardwareInfo.Items.Add(info);
+            }
+
+            if (result.Count > 0)
+            {
+                foreach (string item in result)
+                {
+                    listBoxHardwareInfo.Items.Add(item);
+                }
+            }
+
+            listBoxHardwareInfo.Items.Add(Environment.NewLine);
+        }
+        private void ClearInformation()
+        {
+            // Очищення інформації на формі
+            listBoxHardwareInfo.Items.Clear();
+        }
+    }
+}
